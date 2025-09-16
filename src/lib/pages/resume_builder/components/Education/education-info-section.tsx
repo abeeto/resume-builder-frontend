ab767654468b4be103d2326f7574d49816397d3d
abeeto <59943893+abeeto@users.noreply.github.com>
2025-09-16 16:19:06 +0530

    feat: added delete button

---
+++ src/lib/pages/resume_builder/components/Education/education-info-section.tsx
@@ -0,0 +1,40 @@
+import {
+  type Education,
+  useResume,
+} from '@/lib/pages/resume_builder/contexts/resume-context';
+
+const EducationItem = ({ education }: { education: Education }) => {
+  return (
+    <div className="px-10 mx-auto flex justify-between">
+      <div className="basis-1/3 flex font-light text-base">
+        <div>
+          {education.start_date} -{' '}
+          {education.end_date ? education.end_date : 'Present'}
+        </div>
+      </div>
+
+      <div className="flex-grow basis-auto shrink-1">
+        <div className="font-semibold text-lg">{education.degree}</div>
+        <div className="font-light text-sm">{education.location}</div>
+      </div>
+    </div>
+  );
+};
+
+export const EducationInfoSection = () => {
+  const { state } = useResume();
+  const educations = state.educations;
+
+  return (
+    <div className="px-10 my-10 space-y-5">
+      {educations.length > 0 && (
+        <div className="text-center bg-gray-200 py-1 w-full font-bold text-xl">
+          Education
+        </div>
+      )}
+      {educations.map((education) => (
+        <EducationItem key={education.id} education={education} />
+      ))}
+    </div>
+  );
+};
